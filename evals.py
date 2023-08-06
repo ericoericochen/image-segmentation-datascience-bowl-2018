dcb5ed12092003eef90d3b5fa58646bd9a3dd1f0
Eric Chen <deeplearningericchen@gmail.com>
2023-08-06 10:00:51 -0700

    wrote mean accuracy

--- evals.py
+++ evals.py
@@ -31,12 +31,56 @@ def pixel_accuracy(model: nn.Module, loader: DataLoader) -> float:
     return acc
 
 
+def mean_accuracy(model: nn.Module, loader: DataLoader, num_classes=2):
+    """
+    Evaluates mean accuracy of the predictions by model on data from loader.
+    Mean Accuracy = (1 / # classes) * sum(pixel accuracy per class)
+    """
+    model.eval()
+
+    correct = torch.zeros(num_classes)
+    pixels = torch.zeros(num_classes) + 1e-6
+
+    with torch.no_grad():
+        for X, Y in loader:
+            pred = model(X)
+            mask = inference.get_mask(pred)
+
+            # loop over each class and count pixels + correctly predicted
+            for i in range(num_classes):
+                class_mask = Y == i
+                num_pixels = class_mask.sum()
+                mask_pixels = mask[class_mask]
+                correct_pixels = (mask_pixels == i).sum()
+
+                pixels[i] += num_pixels.item()
+                correct[i] += correct_pixels.item()
+
+    acc = correct / pixels
+    mean_acc = acc.mean()
+
+    return mean_acc.item()
+
+
+def mean_IU(model: nn.Module, loader: DataLoader, num_classes=2):
+    pass
+
+
+def frequency_weighted_IU(model: nn.Module, loader: DataLoader, num_classes=2):
+    pass
+
+
 if __name__ == "__main__":
-    model = lambda x: torch.tensor([[[[0, 100], [0, 0]], [[100, 0], [100, 100]]]])
+    # pixel accuracy
+    model = lambda x: torch.tensor([[[[0, 100], [0, 100]], [[100, 0], [100, 0]]]])
     model.eval = lambda: True
 
-    loader = [(torch.rand(1, 1, 2, 2), torch.tensor([[[1, 1], [1, 1]]]))]
+    loader = [(torch.rand(1, 1, 2, 2), torch.tensor([[[1, 0], [1, 1]]]))]
 
     pixel_acc = pixel_accuracy(model, loader)
 
     assert pixel_acc == 0.75
+
+    # mean accuracy
+    mean_acc = mean_accuracy(model, loader)
+    assert torch.isclose(torch.tensor(mean_acc), torch.tensor(5 / 6))
